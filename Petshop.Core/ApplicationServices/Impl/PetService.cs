0293111de96bff7942a344639e48af84c40e4860
Troels <31724037+tr0els@users.noreply.github.com>
2020-09-21 20:58:58 +0200

    Pet CRUD with Exceptions

--- Petshop.Core/ApplicationServices/Impl/PetService.cs
+++ Petshop.Core/ApplicationServices/Impl/PetService.cs
@@ -23,20 +23,32 @@ namespace Petshop.Core.ApplicationServices.Impl
 
         public Pet Read(int id)
         {
-            return _petRepository.Read(id);
+            if (id < 1) throw new ArgumentException($"Id must be a positive integer.");
+            Pet pet = _petRepository.Read(id);
+            if (pet is null) throw new KeyNotFoundException($"No record with id {id} was found.");
+            return pet;
         }
 
         public Pet Create(Pet pet)
         {
+            ValidatePetData(pet);
             return _petRepository.Create(pet);
         }
         public Pet Update(Pet pet)
         {
+            if (pet.Id < 1) throw new ArgumentException($"Id must be a positive integer.");
+            ValidatePetData(pet);
+            Pet petUpdated = _petRepository.Update(pet);
+            if (petUpdated is null) throw new KeyNotFoundException($"No record with id {pet.Id} was found.");
+
             return _petRepository.Update(pet);
         }
 
         public Pet Delete(int id)
         {
+            if (id < 1) throw new ArgumentException($"Id must be a positive integer.");
+            Pet pet = _petRepository.Delete(id);
+            if (pet is null) throw new KeyNotFoundException($"No record with id {id} was found.");
             return _petRepository.Delete(id);
         }
 
@@ -54,5 +66,16 @@ namespace Petshop.Core.ApplicationServices.Impl
         {
             return _petRepository.TopXCheapest(pets, amount);
         }
-    }
+
+        private void ValidatePetData(Pet pet)
+        {
+            if (string.IsNullOrEmpty(pet.Name) || pet.Name.Length< 2) throw new ArgumentException("Name must be minimum 2 characters long.");
+            if (!Enum.IsDefined(typeof(PetType), pet.Type)) throw new ArgumentException("The pet type is invalid.");
+            if (!DateTime.TryParse(pet.BirthDate.ToString(), out _)) throw new ArgumentException("Birth date invalid format."); // service check - api controller validates date first
+            if (DateTime.Compare(pet.BirthDate, DateTime.Now) > 0) throw new ArgumentException("Birth date must be in the past.");
+            if (!DateTime.TryParse(pet.SoldDate.ToString(), out _)) throw new ArgumentException("Sold date invalid format."); // service check - api controller validates date first
+            if (DateTime.Compare(pet.SoldDate, DateTime.Now) > 0) throw new ArgumentException("Sold date must be in the past.");
+            if (pet.Price< 0) throw new ArgumentException("Price must be an integer greater than or equal to 0.");
+        }
+}
 }
--- Petshop.UI.WebApp/Controllers/PetsController.cs
+++ Petshop.UI.WebApp/Controllers/PetsController.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Petshop.Core.ApplicationServices;
 using Petshop.Core.Entity;
-using Microsoft.Extensions.DependencyInjection;
-using Petshop.Infrastructure.Data;
 
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -26,45 +23,92 @@ namespace Petshop.UI.WebApp.Controllers
 
         // GET: api/pets
         [HttpGet]
-        public IEnumerable<Pet> Get()
+        public ActionResult<IEnumerable<Pet>> Get()
         {
-            return _petService.ReadAll();
+            try
+            {
+                IEnumerable<Pet> pets = _petService.ReadAll();
+
+                if (!pets.Any()) return NoContent();
+                return Ok(pets);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
         }
 
         // GET api/pets/5
         [HttpGet("{id}")]
-        public Pet Get(int id)
+        public ActionResult<Pet> Get(int id)
         {
-            return _petService.Read(id);
+            try {
+                return Ok(_petService.Read(id));
+            }
+            catch (KeyNotFoundException e)
+            {
+                return NotFound(e.Message); // NoContent would be more correct?
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
         }
 
         // POST api/pets
         [HttpPost]
-        public Pet Post([FromBody] Pet pet)
+        public ActionResult<Pet> Post([FromBody] Pet pet)
         {
-            Pet petCreated = _petService.Create(pet);
-            return petCreated;
+            try
+            {
+                Pet petCreated = _petService.Create(pet);
+                return Created($"Pet with id {petCreated.Id} was created.", petCreated);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
         }
 
         // PUT api/pets/5
         [HttpPut("{id}")]
-        public Pet Put(int id, [FromBody] Pet pet)
+        public ActionResult<Pet> Put(int id, [FromBody] Pet pet)
         {
-            // update pet with id from url
+            // use id from url over any potential id from object
             pet.Id = id;
 
-            Pet petUpdated = _petService.Update(pet);
-            return petUpdated;
-            
-
+            try
+            {
+                Pet petUpdated = _petService.Update(pet);
+                return Accepted($"Pet with id {petUpdated.Id} was updated.", petUpdated);
+            }
+            catch (KeyNotFoundException e)
+            {
+                return NotFound(e.Message);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
         }
 
         // DELETE api/pets/5
         [HttpDelete("{id}")]
-        public Pet Delete(int id)
+        public ActionResult<Pet> Delete(int id)
         {
-            Pet petDeleted = _petService.Delete(id);
-            return petDeleted;
+            try
+            {
+                Pet petDeleted = _petService.Delete(id);
+                return Accepted($"Pet with id {petDeleted.Id} was deleted.", petDeleted);
+            }
+            catch (KeyNotFoundException e)
+            {
+                return NotFound(e.Message);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
         }
     }
 }
